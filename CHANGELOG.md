2b38657300ece02ba90b65bcdd16592e4ea2f88f
Clay Dunston <dunstontc@gmail.com>
2018-02-09 22:14:56 -0600

    0.0.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Change Log
 
+## 0.0.6
+- Highlight JS doc comments.
+
 ## 0.0.5
 - Fixed compile error.
 
--- src/partials/javascript.js
+++ src/partials/javascript.js
@@ -1,6 +1,15 @@
 module.exports = [
     {
         "scope": [
+            "comment.block.documentation.js",
+            "punctuation.quasi.element.end.js"
+        ],
+        "settings": {
+            "foreground": "#608b4e"
+        }
+    },
+    {
+        "scope": [
             "punctuation.quasi.element.begin.js",
             "punctuation.quasi.element.end.js"
         ],
@@ -9,7 +18,7 @@ module.exports = [
         }
     },
     {
-        "scope": [ 
+        "scope": [
             "keyword.operator.typeof.js",
             "keyword.operator.logical.js",
             "keyword.operator.in.js",
