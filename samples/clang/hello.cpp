13cc5013831b008820278e4630fba6fdcd409a6b
Clay Dunston <dunstontc@gmail.com>
2018-05-11 13:08:10 -0500

    c++ samples

---
+++ samples/clang/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// using namespace std;
+
+int main() {
+   std::cout << "Hello, world!" << std::endl;
+   return 0;
+}
+
+
+// meta.preprocessor.include.c string.quoted.other.lt-gt.include.c
+// source.cpp meta.preprocessor.include.c string.quoted.other.lt-gt.include.c punctuation.definition.string.begin.c
+// source.cpp meta.preprocessor.include.c string.quoted.other.lt-gt.include.c punctuation.definition.string.begin.c
+// source.cpp meta.preprocessor.include.c string.quoted.other.lt-gt.include.c punctuation.definition.string.begin.c
