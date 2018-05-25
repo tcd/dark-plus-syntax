e22b13ed126bc8adc7f966a3eacf41301713d6b3
Clay Dunston <dunstontc@gmail.com>
2018-05-25 10:49:55 -0500

    trim trailing commas in regex.js

--- src/partials/regex.js
+++ src/partials/regex.js
@@ -9,40 +9,40 @@ module.exports = [
             "keyword.operator.quantifier.regexp",
             "keyword.operator.quantifier.regexp.js",
         ],
-        "settings": { "foreground": "#b5cea8", },
+        "settings": { "foreground": "#b5cea8" },
     },
     {
         "scope": [
             "string.regexp",
-            "constant.regexp",
-            "constant.character.regexp",
+            // "constant.regexp",
+            // "constant.character.regexp",
         ],
-        "settings": { "foreground": "#d16969", },
+        "settings": { "foreground": "#d16969" },
     },
     {
         "scope": [
             "string.regexp constant.character.escape.backslash.regexp",
         ],
-        "settings": { "foreground": "#ce9178", },
+        "settings": { "foreground": "#ce9178" },
     },
     {
         "scope": [
             "constant.other.character-class.regexp",
         ],
-        "settings": { "foreground": "#4ec9b0", },
+        "settings": { "foreground": "#4ec9b0" },
     },
     {
         "scope": [
             "keyword.control.anchor.regexp",
             "constant.other.character-class.regexp",
         ],
-        "settings": { "foreground": "#569cd6", },
+        "settings": { "foreground": "#569cd6" },
     },
     {
         "scope": [
             "punctuation.definition.group.regexp",
             "punctuation.definition.character-class.regexp",
         ],
-        "settings": { "foreground": "#909090", },
+        "settings": { "foreground": "#909090" },
     },
 ];
