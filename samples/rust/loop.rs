5ad1c8c19b3fe76e049c929360789814924215ee
Clay Dunston <dunstontc@gmail.com>
2019-04-01 15:19:36 -0500

    update samples

---
+++ samples/rust/loop.rs
@@ -0,0 +1,22 @@
+fn main() {
+    loop {
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    assert_eq!(result, 20);
+}
