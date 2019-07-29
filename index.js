6175deaa3f189b8d8360f706abc82216ecf78690
Clay Dunston <claydunston@gmail.com>
2019-07-29 15:01:09 -0500

    Extract UI colors into their own file

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ const swift      = require('./src/partials/swift');
 const todo       = require('./src/partials/todo');
 const terraform  = require('./src/partials/terraform');
 
+const ui_colors  = require('./src/partials/ui-colors');
+
 let nop = [];
 let tokenz = nop.concat(
     defaults,
@@ -50,96 +52,7 @@ let tokenz = nop.concat(
 let base = {
     "name": "dark-plus-syntax",
     "type": "dark",
-    "colors": {
-        "list.dropBackground": "#383b3d",
-
-        "editor.background": "#1e1e1e",
-        "editor.foreground": "#d4d4d4",
-        "editor.selectionHighlightBackground": "#add6ff26",
-
-        "editor.inactiveSelectionBackground": "#3a3d41",
-
-        "editorIndentGuide.background": "#404040",
-
-        "activityBarBadge.background": "#007acc",
-
-        "sideBarTitle.foreground": "#bbbbbb",
-
-        "diffEditor.insertedTextBorder": "#608b4e",
-        "diffEditor.removedTextBorder": "#d16969",
-
-        // "editor.findMatchBackground": "#cc00b1",
-        // "editor.findMatchHighlightBackground": "#007acc",
-
-        "editorGutter.addedBackground": "#608b4e",
-        "editorGutter.modifiedBackground": "#d7ba7d",
-        "editorGutter.deletedBackground": "#d16969",
-
-        "editorOverviewRuler.addedForeground": "#608b4e",
-        "editorOverviewRuler.modifiedForeground": "#d7ba7d",
-        "editorOverviewRuler.deletedForeground": "#d16969",
-
-        // "editorOverviewRuler.border": "", // Color of the overview ruler border.
-        // "editorOverviewRuler.findMatchForeground": "", // Overview ruler marker color for find matches. The color must not be opaque to not hide underlying decorations.
-        // "editorOverviewRuler.rangeHighlightForeground": "", //Overview ruler marker color for highlighted ranges, like by the Quick Open, Symbol in File and Find features. The color must not be opaque to not hide underlying decorations.
-        // "editorOverviewRuler.selectionHighlightForeground": "", //Overview ruler marker color for selection highlights. The color must not be opaque to not hide underlying decorations.
-        // "editorOverviewRuler.wordHighlightForeground": "", //Overview ruler marker color for symbol highlights. The color must not be opaque to not hide underlying decorations.
-        // "editorOverviewRuler.wordHighlightStrongForeground": "", // Overview ruler marker color for write-access symbol highlights. The color must not be opaque to not hide underlying decorations.
-        // "editorOverviewRuler.modifiedForeground": "", // Overview ruler marker color for modified content.
-        // "editorOverviewRuler.addedForeground": "", // Overview ruler marker color for added content.
-        // "editorOverviewRuler.deletedForeground": "", // Overview ruler marker color for deleted content.
-        // "editorOverviewRuler.errorForeground": "", // Overview ruler marker color for errors.
-        // "editorOverviewRuler.warningForeground": "", // Overview ruler marker color for warnings.
-        // "editorOverviewRuler.infoForeground": "", // Overview ruler marker color for infos.
-
-        // "editorError.foreground": "#d16969",
-        // "editorError.border": "#d16969",
-        // "editorWarning.foreground": "#d7ba7d",
-        // "editorWarning.border": "#d7ba7d",
-        // "editorInfo.foreground": "#608b4e",
-        // "editorInfo.border": "#608b4e",
-
-        // "notificationCenter.border": "", // Notification Center border color.
-        // "notificationCenterHeader.foreground": "", // Notification Center header foreground color.
-        // "notificationCenterHeader.background": "", // Notification Center header background color.
-        // "notificationToast.border": "", // Notification toast border color.
-        // "notifications.foreground": "", // Notification foreground color.
-        // "notifications.background": "", // Notification background color.
-        // "notifications.border": "", // Notification border color separating from other notifications in the Notification Center.
-        // "notificationLink.foreground": "", // Notification links foreground color.
-
-        "gitDecoration.modifiedResourceForeground": "#608b4e",
-        "gitDecoration.deletedResourceForeground": "#d7ba7d",
-        "gitDecoration.untrackedResourceForeground": "#d7ba7d",
-        "gitDecoration.ignoredResourceForeground": "#808080",
-        "gitDecoration.conflictingResourceForeground": "#d16969",
-
-        // "terminal.background": "#1e1e1e",
-        // "terminal.foreground": "#d4d4d4",
-        // "terminal.ansiBlack": "#808080",
-        // "terminal.ansiBrightBlack": "#808080",
-        // "terminal.ansiRed": "#d16969",
-        // "terminal.ansiBrightRed": "#d16969",
-        // "terminal.ansiBlue": "#569cd6",
-        // "terminal.ansiBrightBlue": "#569cd6",
-        // "terminal.ansiGreen": "#608b4e",
-        // "terminal.ansiBrightGreen": "#608b4e",
-        // "terminal.ansiCyan": "#4ec9b0",
-        // "terminal.ansiBrightCyan": "#4ec9b0",
-        // "terminal.ansiYellow": "#d7ba7d",
-        // "terminal.ansiBrightYellow": "#d7ba7d",
-        // "terminal.ansiMagenta": "#c586c0",
-        // "terminal.ansiBrightMagenta": "#c586c0",
-        // "terminal.ansiWhite": "#d4d4d4",
-        // "terminal.ansiBrightWhite": "#d4d4d4",
-        // "terminal.selectionBackground": "#add6ff26",
-        // "terminalCursor.foreground": "#808080",
-
-        // "statusBar.background": "#303030",
-        // "statusBar.noFolderBackground": "#303030",
-        // "statusBar.debuggingBackground": "#303030",
-    },
-
+    "colors": ui_colors,
     "tokenColors": tokenz,
 };
 
---
+++ src/partials/ui-colors.js
@@ -0,0 +1,106 @@
+module.exports = {
+    // "statusBar.background": "#303030",
+    // "statusBar.noFolderBackground": "#303030",
+    // "statusBar.debuggingBackground": "#303030",
+
+    "list.dropBackground": "#383b3d",
+
+    "editor.background": "#1e1e1e",
+    "editor.foreground": "#d4d4d4",
+    "editor.selectionHighlightBackground": "#add6ff26",
+    "editor.inactiveSelectionBackground": "#3a3d41",
+    "editorIndentGuide.background": "#404040",
+    "activityBarBadge.background": "#007acc",
+    "sideBarTitle.foreground": "#bbbbbb",
+    "diffEditor.insertedTextBorder": "#608b4e",
+    "diffEditor.removedTextBorder": "#d16969",
+    // "editor.findMatchBackground": "#cc00b1",
+    // "editor.findMatchHighlightBackground": "#007acc",
+    "editorGutter.addedBackground": "#608b4e",
+    "editorGutter.modifiedBackground": "#d7ba7d",
+    "editorGutter.deletedBackground": "#d16969",
+    "editorOverviewRuler.addedForeground": "#608b4e",
+    "editorOverviewRuler.modifiedForeground": "#d7ba7d",
+    "editorOverviewRuler.deletedForeground": "#d16969",
+
+    // "editorOverviewRuler.border": "", // Color of the overview ruler border.
+    // "editorOverviewRuler.findMatchForeground": "", // Overview ruler marker color for find matches. The color must not be opaque to not hide underlying decorations.
+    // "editorOverviewRuler.rangeHighlightForeground": "", //Overview ruler marker color for highlighted ranges, like by the Quick Open, Symbol in File and Find features. The color must not be opaque to not hide underlying decorations.
+    // "editorOverviewRuler.selectionHighlightForeground": "", //Overview ruler marker color for selection highlights. The color must not be opaque to not hide underlying decorations.
+    // "editorOverviewRuler.wordHighlightForeground": "", //Overview ruler marker color for symbol highlights. The color must not be opaque to not hide underlying decorations.
+    // "editorOverviewRuler.wordHighlightStrongForeground": "", // Overview ruler marker color for write-access symbol highlights. The color must not be opaque to not hide underlying decorations.
+    // "editorOverviewRuler.modifiedForeground": "", // Overview ruler marker color for modified content.
+    // "editorOverviewRuler.addedForeground": "", // Overview ruler marker color for added content.
+    // "editorOverviewRuler.deletedForeground": "", // Overview ruler marker color for deleted content.
+    // "editorOverviewRuler.errorForeground": "", // Overview ruler marker color for errors.
+    // "editorOverviewRuler.warningForeground": "", // Overview ruler marker color for warnings.
+    // "editorOverviewRuler.infoForeground": "", // Overview ruler marker color for infos.
+
+    // "notificationCenter.border": "", // Notification Center border color.
+    // "notificationCenterHeader.foreground": "", // Notification Center header foreground color.
+    // "notificationCenterHeader.background": "", // Notification Center header background color.
+    // "notificationToast.border": "", // Notification toast border color.
+    // "notifications.foreground": "", // Notification foreground color.
+    // "notifications.background": "", // Notification background color.
+    // "notifications.border": "", // Notification border color separating from other notifications in the Notification Center.
+    // "notificationLink.foreground": "", // Notification links foreground color.
+
+    // ========================================================================
+    // Error Squiggles & Box Border Colors
+    // ========================================================================
+
+    "editorError.foreground": "#d16969",
+    // "editorError.border": "#d16969",
+    "editorWarning.foreground": "#d7ba7d",
+    // "editorWarning.border": "#d7ba7d",
+    "editorInfo.foreground": "#608b4e",
+    // "editorInfo.border": "#608b4e",
+
+    // ========================================================================
+    // Git Colors
+    // ========================================================================
+
+    "gitDecoration.modifiedResourceForeground": "#608b4e",
+    "gitDecoration.addedResourceForeground": "#608b4e",
+    "gitDecoration.deletedResourceForeground": "#d7ba7d",
+    "gitDecoration.untrackedResourceForeground": "#d7ba7d",
+    "gitDecoration.ignoredResourceForeground": "#808080",
+    "gitDecoration.conflictingResourceForeground": "#d16969",
+    // "gitDecoration.submoduleResourceForeground": "",
+
+    // "merge.currentHeaderBackground": "",                 // Current header background in inline merge conflicts. The color must not be opaque so as not to hide underlying decorations.
+    // "merge.currentContentBackground": "",                // Current content background in inline merge conflicts. The color must not be opaque so as not to hide underlying decorations.
+    // "merge.incomingHeaderBackground": "",                // Incoming header background in inline merge conflicts. The color must not be opaque so as not to hide underlying decorations.
+    // "merge.incomingContentBackground": "",               // Incoming content background in inline merge conflicts. The color must not be opaque so as not to hide underlying decorations.
+    // "merge.border": "",                                  // Border color on headers and the splitter in inline merge conflicts.
+    // "merge.commonContentBackground": "",                 // Common ancestor content background in inline merge-conflicts. The color must not be opaque so as not to hide underlying decorations.
+    // "merge.commonHeaderBackground": "",                  // Common ancestor header background in inline merge-conflicts. The color must not be opaque so as not to hide underlying decorations.
+    // "editorOverviewRuler.currentContentForeground": "",  // Current overview ruler foreground for inline merge conflicts.
+    // "editorOverviewRuler.incomingContentForeground": "", // Incoming overview ruler foreground for inline merge conflicts.
+    // "editorOverviewRuler.commonContentForeground": "",   // Common ancestor overview ruler foreground for inline merge conflicts.
+
+    // ========================================================================
+    // Terminal Colors
+    // ========================================================================
+
+    // "terminal.background": "#1e1e1e",
+    // "terminal.foreground": "#d4d4d4",
+    // "terminal.ansiBlack": "#808080",
+    // "terminal.ansiBrightBlack": "#808080",
+    // "terminal.ansiRed": "#d16969",
+    // "terminal.ansiBrightRed": "#d16969",
+    // "terminal.ansiBlue": "#569cd6",
+    // "terminal.ansiBrightBlue": "#569cd6",
+    // "terminal.ansiGreen": "#608b4e",
+    // "terminal.ansiBrightGreen": "#608b4e",
+    // "terminal.ansiCyan": "#4ec9b0",
+    // "terminal.ansiBrightCyan": "#4ec9b0",
+    // "terminal.ansiYellow": "#d7ba7d",
+    // "terminal.ansiBrightYellow": "#d7ba7d",
+    // "terminal.ansiMagenta": "#c586c0",
+    // "terminal.ansiBrightMagenta": "#c586c0",
+    // "terminal.ansiWhite": "#d4d4d4",
+    // "terminal.ansiBrightWhite": "#d4d4d4",
+    // "terminal.selectionBackground": "#add6ff26",
+    // "terminalCursor.foreground": "#808080",
+}
