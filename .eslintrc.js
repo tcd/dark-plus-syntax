4f97ad2263a434eef83506530c0ec1402dcb7501
Clay Dunston <claydunston@gmail.com>
2019-12-05 10:57:36 -0600

    [TOOLING] editorconfig & eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,26 @@
 module.exports = {
-    "root": true,
-    "env": { "node": true },
-    "parserOptions": {
-      "ecmaVersion": 2017,
-      "ecmaFeatures": { "jsx": false },
-      "sourceType": "module"
+  "root": true,
+  "env": {
+    "node": true,
+  },
+  "parserOptions": {
+    "ecmaVersion": 2017,
+    "ecmaFeatures": {
+      "jsx": false
     },
-    "rules": {
-      "comma-dangle": ["error", "always-multiline"],
-      "curly": "error",
-      "eqeqeq": "off",
-      "id-length": ["error", { "min": 3 }],
-    //   "indent": ["error", 4],
-      "indent": "off",
-      "linebreak-style": ["error", "unix"],
-      "no-inline-comments": "off",
-      "no-console": "off",
-      "quotes": "off",
-      "semi": ["error", "always"],
-    }
+    "sourceType": "module"
+  },
+  "rules": {
+    "comma-dangle": ["error", "always"],
+    "curly": "error",
+    "eqeqeq": "off",
+    "id-length": ["error", { "min": 3 }],
+      // "indent": ["error", 4],
+    "indent": "off",
+    "linebreak-style": ["error", "unix"],
+    "no-inline-comments": "off",
+    "no-console": "off",
+    "quotes": "off",
+    "semi": ["error", "always"],
   }
+}
