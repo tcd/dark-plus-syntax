1e935c0d9448fc611693887a43e3028169ae14bd
Clay Dunston <claydunston@gmail.com>
2022-02-07 15:06:32 -0600

    [WIP] Semantic token colors

---
+++ src/theme/semantic.ts
@@ -0,0 +1,8 @@
+import { SemanticColor } from "@lib"
+
+export const semanticTokenColors = {
+    "newOperator":   SemanticColor.FlowControl,
+    "stringLiteral": SemanticColor.Strings,
+    "customLiteral": "#DCDCAA",
+    "numberLiteral": SemanticColor.Numbers,
+}
