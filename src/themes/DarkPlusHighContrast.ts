7048604d4217a116106957231a94eadb0d058f0e
Clay Dunston <claydunston@gmail.com>
2022-02-08 09:54:37 -0600

    [WIP] consistent UI colors

--- src/themes/DarkPlusHighContrast.ts
+++ src/themes/DarkPlusHighContrast.ts
@@ -1,9 +1,11 @@
 import { ThemeSettings, ThemeType } from "@types"
 import { DarkPlusHighContrastPalette } from "@src/palettes"
+import { ui } from "@src/theme/dark"
 
 export const DarkPlusHighContrast: ThemeSettings = {
     name:     "dark-plus-syntax (high contrast)",
     fileName: "dark-plus-syntax-high-contrast",
     type:     ThemeType.HighContrast,
     palette:  DarkPlusHighContrastPalette,
+    uiColors: ui,
 }
--- src/themes/LightPlusHighContrast.ts
+++ src/themes/LightPlusHighContrast.ts
@@ -1,9 +1,11 @@
 import { ThemeSettings, ThemeType } from "@types"
 import { LightPlusHighContrastPalette } from "@src/palettes"
+import { ui } from "@src/theme/light"
 
 export const LightPlusHighContrast: ThemeSettings = {
     name:     "light-plus-syntax (high contrast)",
     fileName: "light-plus-syntax-high-contrast",
     type:     ThemeType.HighContrast,
     palette:  LightPlusHighContrastPalette,
+    uiColors: ui,
 }
