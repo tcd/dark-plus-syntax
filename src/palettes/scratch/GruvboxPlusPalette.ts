7a93dfaf6650ce557fb1249fb32f795c26672d00
Clay Dunston <claydunston@gmail.com>
2022-02-10 08:35:02 -0600

    [WIP] palette swapping

---
+++ src/palettes/scratch/GruvboxPlusPalette.ts
@@ -0,0 +1,112 @@
+import { ThemeColorPalette } from "@types"
+
+export const GruvboxPlusDarkPalette: ThemeColorPalette = {
+    // Semantic
+    RegExp:      "",
+    Strings:     "",
+    Chars:       "",
+    Types:       "",
+    Variables:   "",
+    Functions:   "",
+    Numbers:     "",
+    Keywords:    "",
+    FlowControl: "#fb4934",
+    Comments:    "",
+    DocComments: "",
+    Tags:        "",
+    Noise:       "",
+    // UI
+    Foreground:  "",
+    Background:  "",
+    Selection:   "",
+    Primary:     "",
+    Secondary:   "",
+    Error:       "",
+    Warning:     "",
+    Info:        "",
+    Success:     "",
+}
+
+export const GruvboxPlusLightPalette: ThemeColorPalette = {
+    // Semantic
+    RegExp:      "",
+    Strings:     "",
+    Chars:       "",
+    Types:       "",
+    Variables:   "",
+    Functions:   "",
+    Numbers:     "",
+    Keywords:    "",
+    FlowControl: "#fb4934",
+    Comments:    "",
+    DocComments: "",
+    Tags:        "",
+    Noise:       "",
+    // UI
+    Foreground:  "",
+    Background:  "",
+    Selection:   "",
+    Primary:     "",
+    Secondary:   "",
+    Error:       "",
+    Warning:     "",
+    Info:        "",
+    Success:     "",
+}
+
+let dark0_hard  = "#1d2021"     // 29-32-33
+let dark0       = "#282828"     // 40-40-40
+let dark0_soft  = "#32302f"     // 50-48-47
+let dark1       = "#3c3836"     // 60-56-54
+let dark2       = "#504945"     // 80-73-69
+let dark3       = "#665c54"     // 102-92-84
+let dark4       = "#7c6f64"     // 124-111-100
+let dark4_256   = "#7c6f64"     // 124-111-100
+
+let gray_245    = "#928374"     // 146-131-116
+let gray_244    = "#928374"     // 146-131-116
+
+let light0_hard = "#f9f5d7"     // 249-245-215
+let light0      = "#fbf1c7"     // 253-244-193
+let light0_soft = "#f2e5bc"     // 242-229-188
+let light1      = "#ebdbb2"     // 235-219-178
+let light2      = "#d5c4a1"     // 213-196-161
+let light3      = "#bdae93"     // 189-174-147
+let light4      = "#a89984"     // 168-153-132
+let light4_256  = "#a89984"     // 168-153-132
+
+let bright_red     = "#fb4934"     // 251-73-52
+let bright_green   = "#b8bb26"     // 184-187-38
+let bright_yellow  = "#fabd2f"     // 250-189-47
+let bright_blue    = "#83a598"     // 131-165-152
+let bright_purple  = "#d3869b"     // 211-134-155
+let bright_aqua    = "#8ec07c"     // 142-192-124
+let bright_orange  = "#fe8019"     // 254-128-25
+
+let neutral_red    = "#cc241d"     // 204-36-29
+let neutral_green  = "#98971a"     // 152-151-26
+let neutral_yellow = "#d79921"     // 215-153-33
+let neutral_blue   = "#458588"      // 69-133-136
+let neutral_purple = "#b16286"     // 177-98-134
+let neutral_aqua   = "#689d6a"      // 104-157-106
+let neutral_orange = "#d65d0e"     // 214-93-14
+
+let faded_red      = "#9d0006"      // 157-0-6
+let faded_green    = "#79740e"     // 121-116-14
+let faded_yellow   = "#b57614"     // 181-118-20
+let faded_blue     = "#076678"      // 7-102-120
+let faded_purple   = "#8f3f71"      // 143-63-113
+let faded_aqua     = "#427b58"      // 66-123-88
+let faded_orange   = "#af3a03"     // 175-58-3
+
+let GRUVBOX_COLORS = {
+    DARK:  {},
+    LIGHT: {},
+    GRAY:  {
+        "245": "#928374",     // 146-131-116
+        "244": "#928374",     // 146-131-116
+    },
+    BRIGHT:  {},
+    NEUTRAL: {},
+    FADED:   {},
+}
