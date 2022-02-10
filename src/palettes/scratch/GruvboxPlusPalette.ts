d53b890b54cbf3ddf51c24d98c215cde57a0394b
Clay Dunston <claydunston@gmail.com>
2022-02-10 09:06:53 -0600

    misc

---
+++ src/palettes/scratch/GruvboxPlusPalette.ts
@@ -0,0 +1,28 @@
+import { ThemeColorPalette } from "@types"
+
+export const GruvboxPlusPalette: ThemeColorPalette = {
+    // Semantic
+    RegExp:      "", // "#d16969",
+    Strings:     "", // "#ce9178",
+    Chars:       "", // "#d7ba7d",
+    Types:       "", // "#4ec9b0",
+    Variables:   "", // "#9cdcfe",
+    Functions:   "", // "#dcdcaa",
+    Numbers:     "", // "#b5cea8",
+    Keywords:    "", // "#569cd6",
+    FlowControl: "", // "#c586c0",
+    Comments:    "", // "#505050",
+    DocComments: "", // "#608b4e",
+    Tags:        "", // "#808080",
+    Noise:       "", // "#404040",
+    // UI
+    Foreground:  "", // "#d4d4d4",
+    Background:  "", // "#1e1e1e",
+    Selection:   "", // "#add6ff26",
+    Primary:     "", // "#608b4e",
+    Secondary:   "", // "#007acc",
+    Error:       "", // "#d16969",
+    Warning:     "", // "#d7ba7d",
+    Info:        "", // "#9cdcfe",
+    Success:     "", // "#608b4e",
+}
