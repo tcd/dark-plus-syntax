da7c6d8cc791c1656004ffe55ade4c560b778c12
Clay Dunston <claydunston@gmail.com>
2022-10-18 09:41:34 -0500

    Fix swapped light/light-high-contrast UI overrides

--- src/themes/LightPlus.ts
+++ src/themes/LightPlus.ts
@@ -1,10 +1,10 @@
 import { ThemeSettings, ThemeType } from "@types"
 import { LightPlusPalette } from "@src/palettes"
-import { LightHighContrastUi } from "@src/theme"
+import { LightUi } from "@src/theme"
 
 export const LightPlus: ThemeSettings = {
     name:     "light-plus-syntax",
     type:     ThemeType.Light,
     palette:  LightPlusPalette,
-    uiColors: LightHighContrastUi,
+    uiColors: LightUi,
 }
